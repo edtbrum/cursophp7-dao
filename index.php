15f41c1a316424630adb5b6b177868b151f7e3ea
Eduardo Brum <edtbrum@gmail.com>
2017-10-31 17:01:09 -0200

    Adicionadas funções de lista, search e login

--- index.php
+++ index.php
@@ -6,8 +6,19 @@ require_once("config.php");
 //$usuarios = $sql->select("SELECT * FROM tb_usuarios");
 //echo json_encode($usuarios);
 
-$root = new Usuario();
-$root->loadById(3);
-echo $root;
+//$user = new Usuario();
+//$user->loadById(4);
+//echo $user;
+
+
+$lista = Usuario::getList();
+echo json_encode($lista);
+
+//$search = Usuario::search("jo");
+//echo json_encode($search);
+
+//$usuario = new Usuario();
+//$usuario->login("eduardo","120976");
+//echo $usuario;
 
  ?>
